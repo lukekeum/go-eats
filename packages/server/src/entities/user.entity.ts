c0f6275bf7ff4c10ee4a6831e96d76805278aece
lukekeum <lukekeum11@gmail.com>
2021-02-22 06:28:51 +0900

    fix(database)!: Set email unique to true

--- packages/server/src/entities/user.entity.ts
+++ packages/server/src/entities/user.entity.ts
@@ -9,16 +9,25 @@ import {
 } from 'typeorm';
 import UserProfile from './userProfile.entity';
 
+export enum EUserType {
+  CUSTOMER = 'customer',
+  DELIVERY = 'delivery',
+  MARKETER = 'marketer',
+}
+
 @Entity('users')
 export default class User extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   uuid: string;
 
-  @Column({ nullable: false })
+  @Column({ nullable: false, unique: true })
   email: string;
 
   @Column({ nullable: false })
   password: string;
+
+  @Column({ default: EUserType.CUSTOMER })
+  userType: EUserType;
 
   @OneToOne((type) => UserProfile, (userProfile) => userProfile.user)
   profile: UserProfile;
