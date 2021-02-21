6e74689ebdce96071829f1d5763c8797780c30fb
lukekeum <lukekeum11@gmail.com>
2021-02-21 21:19:18 +0900

    feat(atom)!: Add authState atom
    This auth state atom will store signedIn(boolean),
    and userdata(just email so far) and etc.

---
+++ packages/userclient/src/atom/auth.ts
@@ -0,0 +1,20 @@
+import { atom } from 'recoil';
+
+export interface ISigninState {
+  isLoading: boolean;
+  isSignedIn: boolean;
+  data: {
+    email: null | string;
+  };
+}
+
+export const signinState = atom<ISigninState>({
+  key: 'signin',
+  default: {
+    isLoading: false,
+    isSignedIn: false,
+    data: {
+      email: null,
+    },
+  },
+});
