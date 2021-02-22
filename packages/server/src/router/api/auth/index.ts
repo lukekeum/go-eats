c4eae7343d0faa4dfcc3436d962fcabca0cc51dc
lukekeum <lukekeum11@gmail.com>
2021-02-23 08:57:06 +0900

    feat(router)!: Register silentRefresh router

--- packages/server/src/router/api/auth/index.ts
+++ packages/server/src/router/api/auth/index.ts
@@ -2,10 +2,12 @@ import { FastifyPluginCallback } from 'fastify';
 
 import signUpRoute from './signup';
 import signInRoute from './signin';
+import silentRefresh from './silentRefresh';
 
 const authRoute: FastifyPluginCallback = (fastify, opts, done) => {
   fastify.register(signUpRoute, { prefix: '/signup' });
   fastify.register(signInRoute, { prefix: '/signin' });
+  fastify.register(silentRefresh, { prefix: '/silent-refresh' });
 
   done();
 };
---
+++ packages/server/src/router/api/auth/silentRefresh.ts
@@ -0,0 +1,49 @@
+import AuthToken from '@src/entities/authToken.entity';
+import User from '@src/entities/user.entity';
+import UserProfile from '@src/entities/userProfile.entity';
+import { FastifyPluginCallback } from 'fastify';
+
+interface IUserTokenCookie {
+  user_id: string;
+  token_id: string;
+}
+
+const silentRefresh: FastifyPluginCallback = (fastify, opts, done) => {
+  fastify.post('/', async (req, res) => {
+    const { token: refreshToken } = <{ token: string | undefined }>req.cookies;
+
+    if (!refreshToken) {
+      return res.status(401).send({ message: 'Token not found' });
+    }
+
+    try {
+      const decodedUser = <IUserTokenCookie>fastify.jwt.decode(refreshToken);
+
+      const user = await User.findOne({ uuid: decodedUser.user_id });
+      const userProfile = await UserProfile.findOne({
+        fk_user_id: decodedUser.user_id,
+      });
+
+      if (!decodedUser || !user) {
+        return res.status(401).send({ message: 'Invalid token' });
+      }
+
+      const tokens = await user.generateAuthToken();
+      res.setCookie('token', tokens.refreshToken);
+
+      return res.status(201).send({
+        data: {
+          email: user.email,
+          username: userProfile?.username,
+        },
+        token: tokens.accessToken,
+      });
+    } catch (err) {
+      fastify.log.error(err);
+    }
+  });
+
+  done();
+};
+
+export default silentRefresh;
