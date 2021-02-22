2462c79f06741561c13ef85a81567314e0bf51c9
lukekeum <lukekeum11@gmail.com>
2021-02-23 08:59:44 +0900

    feat(auth)!: Make silent-refresh when refresh

--- packages/userclient/src/App.tsx
+++ packages/userclient/src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import useLogin from './hooks/useLogin';
 import Main from './pages/Main';
 import './styles/response.scss';
 
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+
 function App() {
+  const [, silentFN] = useLogin();
+  useEffect(() => {
+    silentFN();
+  }, [silentFN]);
   return (
     <BrowserRouter>
       <Switch>
