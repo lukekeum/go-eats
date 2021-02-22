7ae3232cf7380267e814767dcf5e5030a985ba7e
lukekeum <lukekeum11@gmail.com>
2021-02-22 14:11:18 +0900

    feat(router)!: Register signin route

--- packages/server/src/router/api/auth/index.ts
+++ packages/server/src/router/api/auth/index.ts
@@ -1,9 +1,11 @@
 import { FastifyPluginCallback } from 'fastify';
 
 import signUpRoute from './signup';
+import signInRoute from './signin';
 
 const authRoute: FastifyPluginCallback = (fastify, opts, done) => {
   fastify.register(signUpRoute, { prefix: '/signup' });
+  fastify.register(signInRoute, { prefix: '/signin' });
 
   done();
 };
---
+++ packages/server/src/router/api/auth/signin.ts
@@ -0,0 +1,50 @@
+import User from '@src/entities/user.entity';
+import { FastifyPluginCallback } from 'fastify';
+import bcrypt from 'bcrypt';
+import app from '@src/app';
+import UserProfile from '@src/entities/userProfile.entity';
+
+interface ISignInBody {
+  email: string;
+  password: string;
+}
+
+const signInRoute: FastifyPluginCallback = (fastify, opts, done) => {
+  fastify.post('/', async (req, res) => {
+    const body = <ISignInBody>req.body;
+
+    if (!body) {
+      return res.status(401).send({ message: 'Unknown input' });
+    }
+
+    try {
+      const user = await User.findOne({ email: body.email });
+
+      if (!user) {
+        return res.status(401).send({ message: 'Email user not found' });
+      }
+
+      const isPasswordCorrect = await bcrypt.compare(
+        body.password,
+        user.password
+      );
+
+      if (!isPasswordCorrect) {
+        return res.status(401).send({ message: 'Password Incorrect' });
+      }
+
+      const userProfile = await UserProfile.findOne({ fk_user_id: user.uuid });
+
+      return res.status(201).send({
+        message: 'Signed in successfully',
+        data: { email: body.email, username: userProfile?.username },
+      });
+    } catch (err) {
+      app.server.log.error(err);
+    }
+  });
+
+  done();
+};
+
+export default signInRoute;
