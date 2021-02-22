7b80805b909d2ffc4b37edb8ed132e5c3554c5e3
lukekeum <lukekeum11@gmail.com>
2021-02-22 16:31:58 +0900

    feat(axios)!: Setup axios default setting

---
+++ packages/userclient/src/lib/client/axios.ts
@@ -0,0 +1,43 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+
+class Axios {
+  private axios: AxiosInstance;
+
+  constructor() {
+    this.axios = axios.create({
+      baseURL: process.env.REACT_APP_BASE_URL,
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  get(path: string, payload = null as any) {
+    return this.axios
+      .get(path, payload)
+      .then((response: AxiosResponse) => response);
+  }
+
+  put(path: string, payload = null as any) {
+    return this.axios
+      .put(path, payload)
+      .then((response: AxiosResponse) => response);
+  }
+
+  post(path: string, payload = null as any) {
+    return this.axios
+      .post(path, payload)
+      .then((response: AxiosResponse) => response);
+  }
+
+  delete(path: string, payload = null as any) {
+    return this.axios
+      .delete(path, payload)
+      .then((response: AxiosResponse) => response);
+  }
+}
+
+const client = new Axios();
+
+export default client;
