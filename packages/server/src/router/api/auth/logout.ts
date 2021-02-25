13aeb44e083e45e56a1559238412406ec4246006
lukekeum <lukekeum11@gmail.com>
2021-02-26 00:05:24 +0900

    feat(logout)!: Disable token when logout

--- packages/server/src/router/api/auth/logout.ts
+++ packages/server/src/router/api/auth/logout.ts
@@ -1,9 +1,37 @@
+import AuthToken from '@src/entities/authToken.entity';
 import { FastifyPluginCallback } from 'fastify';
 
+interface IUserTokenCookie {
+  user_id: string;
+  token_id: string;
+}
+
 const logoutRoute: FastifyPluginCallback = (fastify, opts, done) => {
-  fastify.post('/', (req, res) => {
-    res.setCookie('token', '', { httpOnly: true });
-    res.send(201).send({ message: 'Logged out successfully' });
+  fastify.post('/', async (req, res) => {
+    const { token: refreshToken } = <{ token: string | undefined }>req.cookies;
+
+    if (!refreshToken) {
+      return res.status(401).send({ message: 'Token not found' });
+    }
+
+    try {
+      const decodedToken = <IUserTokenCookie>fastify.jwt.decode(refreshToken);
+
+      const tokenEntity = await AuthToken.findOne(decodedToken.token_id);
+
+      if (!tokenEntity) {
+        return res.status(401).send({ message: 'Invalid Token' });
+      }
+
+      tokenEntity.disabled = true;
+
+      await tokenEntity.save();
+
+      res.setCookie('token', '', { httpOnly: true });
+      res.send(201).send({ message: 'Logged out successfully' });
+    } catch (err) {
+      res.status(501).send({ message: 'Internal Error' });
+    }
   });
 
   done();
