3a789a597d6f9fbb9cfa97188662e303b6e01ed8
lukekeum <lukekeum11@gmail.com>
2021-02-28 12:56:37 +0900

    fix(router)!: Set prefix of meRoute

--- packages/server/src/router/api/user/index.ts
+++ packages/server/src/router/api/user/index.ts
@@ -2,7 +2,7 @@ import { FastifyPluginCallback } from 'fastify';
 import meRoute from './me';
 
 const userRoute: FastifyPluginCallback = (fastify, opts, done) => {
-  fastify.register(meRoute, '/me');
+  fastify.register(meRoute, { prefix: '/me' });
 
   done();
 };
