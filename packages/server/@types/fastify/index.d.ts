f268271f478d249eb5b2c576f6a9b3fcf9a1c5a3
lukekeum <lukekeum11@gmail.com>
2021-03-01 04:01:48 +0900

    fix(type-err)!: Remove reference type from token requestType

--- packages/server/@types/fastify/index.d.ts
+++ packages/server/@types/fastify/index.d.ts
@@ -1,11 +1,7 @@
 import fastify from 'fastify';
 
-interface IAuthToken {
-  user_id: string;
-}
-
 declare module 'fastify' {
   export interface FastifyRequest {
-    token: IAuthToken;
+    token: any;
   }
 }
