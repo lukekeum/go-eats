765698d3baae84f8337dea7dcdf5b1b479ea955d
lukekeum <lukekeum11@gmail.com>
2021-03-05 23:56:49 +0900

    feat(Profile)!: Add Profile component

---
+++ packages/userclient/src/components/Settings/Proile.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Profile() {
+  return <ProfileContainer></ProfileContainer>;
+}
+
+const ProfileContainer = styled.div`
+  width: 100%;
+  & h1 {
+  }
+`;
+
+export default Profile;
