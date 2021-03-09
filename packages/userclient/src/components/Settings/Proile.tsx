3f2a7d3b06ba67b3ea7e85ab84ec25c7c33957ae
lukekeum <lukekeum11@gmail.com>
2021-03-09 22:25:06 +0900

    feat(Profile)!: Add profile component

--- packages/userclient/src/components/Settings/Proile.tsx
+++ packages/userclient/src/components/Settings/Proile.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
+import { signinState } from '../../atom/auth';
+import Hyper from './Hyper';
 
 function Profile() {
-  return <ProfileContainer></ProfileContainer>;
+  const { data } = useRecoilValue(signinState);
+
+  return (
+    <ProfileContainer>
+      <h1>{data.username}</h1>
+      <Hyper>asdf</Hyper>
+    </ProfileContainer>
+  );
 }
 
 const ProfileContainer = styled.div`
   width: 100%;
   & h1 {
+    font-weight: 400;
+    font-size: 2rem;
   }
 `;
 
