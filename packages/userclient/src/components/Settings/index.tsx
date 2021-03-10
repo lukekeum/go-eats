6d28f327c2337ff66fa322c4f44a07f30b41058b
lukekeum <lukekeum11@gmail.com>
2021-03-10 23:55:52 +0900

    feat(Setting)!: Add Profile to main setting component

--- packages/userclient/src/components/Settings/index.tsx
+++ packages/userclient/src/components/Settings/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Profile from './Proile';
 
 function SettingComponent() {
-  return <div></div>;
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
 }
 
 export default SettingComponent;
